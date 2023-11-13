aa6e8eecca93fd02990b26840e7db1705115f781
Axel Salas <missingno12824@gmail.com>
2023-11-13 03:39:07 -0600

    Initial commit

--- person.js
+++ person.js
@@ -1,5 +1,9 @@
-console.log(__dirname, __filename);
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
 
+// })
+
+// console.log(__dirname, __filename);
 class Person {
     constructor(name, age) {
       this.name = name;
